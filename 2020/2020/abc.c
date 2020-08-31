ac7b5b1ceca56d2cf430dbb20cea1ebd9622d3da
bitianjin <50616822+bitianjin@users.noreply.github.com>
2020-08-31 16:53:42 +0800

    Add files via upload

---
+++ 2020/2020/abc.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//包含一个叫stdio.h的文件
+//std-标准 standard input output 
+
+#include <stdio.h>
+
+//int main()//主函数-程序的入口 main函数有且只有一个
+//{	
+	//这里完成任务
+	//在屏幕上输出hello world
+	//函数-print function -printf - 打印函数
+	//库函数-C语言本身提供给我们使用的函数
+	//别人的东西-打招呼
+	//#include
+	//printf("666");
+	//return 0;
+//}
+//int 是整形的意思
+//main前面的int表示main函数调用返回一个整形值
+
+	int main()
+	{
+	  //char ch='A';//内存
+	  //printf("%c\n",ch);//%c--打印字符格式的数据
+	  //short int - 短整型
+	  //int - 整型
+	  //int age=20；
+	  //printf（"%d\n",age）;//%d -- 打印整型十进制数据
+	  //long 长整形
+		long num = 100;
+		printf("%d\n",num);
+	  return 0 ;
+	}
